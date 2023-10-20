b5143205499c7f13557685e28586c127e0aba51d
Camilo <camilovamp@gmail.com>
2023-10-20 17:28:29 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+
+st.header('Market of used cars.Original data')
+st.write("""
+##### Filter the data below to see the ads by manufacturer
+""")
+
